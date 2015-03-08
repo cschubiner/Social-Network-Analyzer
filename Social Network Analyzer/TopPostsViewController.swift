fed44b5de13be681cf86df0bd91e5937ce8ffd24
Raissa Largman <raissa.largman@gmail.com>
2015-03-08 00:38:36 -0800

    Adding third tab to controller

--- Social Network Analyzer/TopPostsViewController.swift
+++ Social Network Analyzer/TopPostsViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class TopPostsViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
