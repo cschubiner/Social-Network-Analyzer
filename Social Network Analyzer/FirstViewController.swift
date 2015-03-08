386e57f06477eb8fffa1d621a12d74fffe0c1b99
Clay Schubiner <cschubiner@gmail.com>
2015-03-07 22:40:09 -0800

    Initial Commit

---
+++ Social Network Analyzer/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Social Network Analyzer
+//
+//  Created by Clayton Schubiner on 3/7/15.
+//  Copyright (c) 2015 Clayton Schubiner. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Social Network AnalyzerTests/Social_Network_AnalyzerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Social_Network_AnalyzerTests.swift
+//  Social Network AnalyzerTests
+//
+//  Created by Clayton Schubiner on 3/7/15.
+//  Copyright (c) 2015 Clayton Schubiner. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Social_Network_AnalyzerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
