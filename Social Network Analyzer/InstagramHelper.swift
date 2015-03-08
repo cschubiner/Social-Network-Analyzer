068d62634c953ba4dc4bbf93364b1669c7941ba3
Clay Schubiner <cschubiner@gmail.com>
2015-03-08 04:27:24 -0700

    instagram ppl likes

---
+++ Social Network Analyzer/InstagramHelper.swift
@@ -0,0 +1,28 @@
+//
+//  InstagramHelper.swift
+//  Social Network Analyzer
+//
+//  Created by Clayton Schubiner on 3/8/15.
+//  Copyright (c) 2015 Douglas Safreno. All rights reserved.
+//
+
+import UIKit
+
+struct InstagramHelper {
+    static func getAllPosts(callback: ([InstagramMedia]) -> ()) {
+        
+        let eng = InstagramEngine.sharedEngine();
+
+        eng.getSelfUserDetailsWithSuccess({ (user : InstagramUser!)  in
+            eng.getMediaForUser(user.Id, withSuccess: { media, paginationInfo in
+                let m = media as [InstagramMedia]!
+                callback(m);
+                }, failure: { (error: NSError!) in
+                    println(error)
+            })
+            }, failure: { error in
+                println("instagram failed");
+                
+        })
+    }
+}
--- Social Network Analyzer/PeopleTableViewController.swift
+++ Social Network Analyzer/PeopleTableViewController.swift
@@ -9,89 +9,107 @@
 import UIKit
 
 class PeopleTableViewController: UITableViewController {
-
+    
+    struct PostLiker {
+        var userName = ""
+        var numLikes = 0
+    }
+    
+    var instagramLikers: [PostLiker] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
-    }
+        
+        let priority = DISPATCH_QUEUE_PRIORITY_DEFAULT
+        dispatch_async(dispatch_get_global_queue(priority, 0), { ()->() in
+            InstagramHelper.getAllPosts() { posts in
+                
+                var instagramLikersDict = [String : PostLiker]()
+                
+                for post: InstagramMedia in posts {
+                    for user in post.likes {
+                        
+                        let user = user as InstagramUser
+                        let userName = user.username
+                        if (instagramLikersDict[userName] == nil) {
+                            var pl = PostLiker()
+                            pl.userName = userName
+                            pl.numLikes = 0
+                            instagramLikersDict[userName] = pl
+                        }
+                        instagramLikersDict[userName]?.numLikes += 1
+                    }
+                }
+                
+                self.instagramLikers = [PostLiker](instagramLikersDict.values)
+                self.instagramLikers.sort({$0.numLikes > $1.numLikes})
+                
 
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.tableView.reloadData()
+                })
+            }
+            
+            
+        })
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Potentially incomplete method implementation.
         // Return the number of sections.
-        return 0
+        return 2
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        return 0
+        if (section == 0) {
+            return 0;
+        }
+        return instagramLikers.count
     }
-
-    /*
+    
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCellWithIdentifier("peopleCell", forIndexPath: indexPath) as UITableViewCell
+        
+        if (indexPath.section == 1) {
+            //instagram
+            
+               let liker = self.instagramLikers[indexPath.row] as PostLiker
+               cell.textLabel?.text = liker.userName + " - " + String(liker.numLikes) + " likes"
+            
+        }
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(tableView: UITableView, shouldHighlightRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return false
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+    
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
