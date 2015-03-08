ab53c1c06a756baef8c48e8bf14d2eaa404ac438
Clay Schubiner <cschubiner@gmail.com>
2015-03-08 04:56:22 -0700

    fixes login problems

--- Social Network Analyzer/LoadingViewController.swift
+++ Social Network Analyzer/LoadingViewController.swift
@@ -15,21 +15,20 @@ class LoadingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        activity.startAnimating()
-        
-        FBSession.openActiveSessionWithReadPermissions(["public_profile, user_photos, read_stream"], allowLoginUI: true, completionHandler: { session, state, error in
-            println("Logged in with Facebook!")
-            println("permissions: \(FBSession.activeSession().accessTokenData.permissions)")
-            dispatch_async(dispatch_get_main_queue()) {
-                self.instagramHasLoaded = true;
-                self.performSegueWithIdentifier("loadingToInstagramController", sender: nil)
-            }
-        })
-        
     }
     
     override func viewDidAppear(animated: Bool) {
         if (!self.instagramHasLoaded) {
+            activity.startAnimating()
+            
+            FBSession.openActiveSessionWithReadPermissions(["public_profile, user_photos, read_stream"], allowLoginUI: true, completionHandler: { session, state, error in
+                println("Logged in with Facebook!")
+                println("permissions: \(FBSession.activeSession().accessTokenData.permissions)")
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.instagramHasLoaded = true;
+                    self.performSegueWithIdentifier("loadingToInstagramController", sender: nil)
+                }
+            })
             return;
         }
         
