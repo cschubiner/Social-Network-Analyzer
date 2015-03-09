4d4d59d6475e377de01130e66fcd4b50b93ba9fd
Clay Schubiner <cschubiner@gmail.com>
2015-03-08 17:05:19 -0700

    sexy login controller

--- Social Network Analyzer/LoadingViewController.swift
+++ Social Network Analyzer/LoadingViewController.swift
@@ -17,14 +17,18 @@ class LoadingViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    @IBOutlet weak var loginLabel: UILabel!
+    
     override func viewDidAppear(animated: Bool) {
         if (!self.instagramHasLoaded) {
-            activity.startAnimating()
             
             FBSession.openActiveSessionWithReadPermissions(["public_profile, user_photos, read_stream"], allowLoginUI: true, completionHandler: { session, state, error in
                 println("Logged in with Facebook!")
                 println("permissions: \(FBSession.activeSession().accessTokenData.permissions)")
                 dispatch_async(dispatch_get_main_queue()) {
+                    
+                    self.loginLabel.text = "Logging in to Instagram..."
+                    
                     FbHelper.startLoadingData()
                     self.instagramHasLoaded = true;
                     self.performSegueWithIdentifier("loadingToInstagramController", sender: nil)
@@ -34,6 +38,7 @@ class LoadingViewController: UIViewController {
         }
         
         if (InstagramEngine.sharedEngine().accessToken != nil) {
+            self.loginLabel.text = "Logging in..."
             println("Logged in with Instagram!")
             self.performSegueWithIdentifier("loadingToTabBarController", sender: nil)
         }
