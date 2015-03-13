b557471c361ff478dff2eec3e12069afa143f748
Douglas Safreno <dsafreno@cs.stanford.edu>
2015-03-12 19:38:58 -0700

    Narrow down graphs view

--- Social Network Analyzer/GraphsViewController.swift
+++ Social Network Analyzer/GraphsViewController.swift
@@ -8,6 +8,10 @@
 
 import UIKit
 
+private let oneWeek = 7.0 * 24 * 60 * 60
+private let times = [oneWeek, oneWeek * 5, oneWeek * 52, oneWeek * 1000]
+private let initVal = 0.0000001
+
 class GraphsViewController: UITableViewController {
     //@IBOutlet weak var postsByHourView: HoursHistogramView!
     //@IBOutlet weak var avgLikesView: HoursHistogramView!
@@ -15,13 +19,20 @@ class GraphsViewController: UITableViewController {
     @IBOutlet weak var avgLikesView: HoursHistogramView!
     @IBOutlet weak var postsByHourView: HoursHistogramView!
     var fbHook: Int?
+    var startTime: NSDate = NSDate(timeIntervalSinceNow: -1 * times.last!) {
+        didSet {
+            postsByHour = [Double](count: 24, repeatedValue: initVal)
+            avgLikesPerHour = [Double](count: 24, repeatedValue: initVal)
+            updateData(FbHelper.allPosts)
+        }
+    }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        var postsByHour = [Double](count: 24, repeatedValue: 0)
-        var avgLikesPerHour = [Double](count: 24, repeatedValue: 0)
-        fbHook = FbHelper.registerCallback() { posts in
-            for post in posts {
+    private var postsByHour = [Double](count: 24, repeatedValue: initVal)
+    private var avgLikesPerHour = [Double](count: 24, repeatedValue: initVal)
+    
+    private func updateData(posts: [FbPost]) {
+        for post in posts {
+            if post.creationTime.compare(startTime) == NSComparisonResult.OrderedDescending {
                 let calendar = NSCalendar.currentCalendar()
                 let components = calendar.components(.CalendarUnitHour | .CalendarUnitMinute, fromDate: post.creationTime)
                 let hour = components.hour
@@ -29,11 +40,18 @@ class GraphsViewController: UITableViewController {
                 postsByHour[hour] += 1
                 avgLikesPerHour[hour] = totalLikes / postsByHour[hour]
             }
-            self.postsByHourView?.data = postsByHour
-            self.avgLikesView?.data = avgLikesPerHour
         }
-
-        // Do any additional setup after loading the view.
+        postsByHourView?.data = postsByHour
+        avgLikesView?.data = avgLikesPerHour
+    }
+    
+    @IBOutlet weak var timeControl: UISegmentedControl!
+    @IBAction func timeChanged() {
+        startTime = NSDate(timeIntervalSinceNow: -1 * times[timeControl.selectedSegmentIndex])
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fbHook = FbHelper.registerCallback(updateData)
     }
     
     deinit {
